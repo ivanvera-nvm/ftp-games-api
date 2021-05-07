96096c0312d371084e47de4647896c0126843ed3
ivanvera-nvm <veraivandamian@gmail.com>
2021-05-06 23:41:22 -0300

    updated

--- src/components/Styled.jsx
+++ src/components/Styled.jsx
@@ -1,35 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/buttons.css";
 import "../styles/neonText.css";
 
+const spanChilds = [1, 2, 3, 4];
+
 export default function Styled() {
   return (
     <div className="body1">
-      <h1 contentEditable="true" className='inter-text-h1'>Test components</h1>
+      <h1 contentEditable="true" className="inter-text-h1">
+        Test components
+      </h1>
 
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        button
-      </a>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        button
-      </a>
+      <Link className="btn-box" href="#">
+        {spanChilds.map((child) => (
+          <span className="btn-spn"></span>
+        ))}
+        button 1
+      </Link>
+      <Link className="btn-box" href="#">
+        {spanChilds.map((child) => (
+          <span className="btn-spn"></span>
+        ))}
+        button 2
+      </Link>
 
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        button
-      </a>
+      <Link className="btn-box" href="#">
+        {spanChilds.map((child) => (
+          <span className="btn-spn"></span>
+        ))}
+        button 3
+      </Link>
     </div>
   );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/main.css";
 
+const siderItems = ["My List", "Top 10", "Reviews"];
+
 function App() {
-
-
   return (
     <div className="App">
-      <header>
+      <header className="navbar">
         <h1>FTP GAMES</h1>
         <ul>
-          <li>Games</li>
-          <li>Top 10 2021</li>
+          <li>
+            <Link to="#">Games</Link>
+          </li>
+          <li>
+            <Link to="#">Top 10 2021</Link>
+          </li>
         </ul>
       </header>
+      <div className="sider">
+        <ul className='sider-list'>
+          {siderItems.map((item, i) => (
+            <li key={i} className="sider-item">
+              <Link className="sider-btn">{item}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
