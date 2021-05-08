957d497883f94af05ac6ed84073ea1fc03d5c5e9
ivanvera-nvm <veraivandamian@gmail.com>
2021-05-08 02:54:20 -0300

    updated

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/list.css";
 
 const list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const filters = ["Platform:", "Genre/Tag:", "Sort by:", "Advanced filter:"];
 
 const GamesList = () => {
   return (
     <div className="list">
+      <span className="filter-container">
+        <ul className="filters">
+          {filters.map((item, i) => (
+            <li>
+              <a>
+                {item}
+                <i className="icon" />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </span>
       {list.map((items, i) => (
         <div key={i} className="list-items">
           <div className="item-section">
-            <div className="img">Image</div>
+            <img
+              className="img"
+              src="https://www.freetogame.com/g/452/thumbnail.jpg"
+              alt="cover"
+            ></img>
             <div className="info">Information</div>
           </div>
         </div>
